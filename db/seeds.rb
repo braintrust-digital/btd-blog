3c402f7708bad9e6bb1bcafccebb56228d803ae1
Thomas Bush <ThomasJosephBush@gmail.com>
2020-01-30 21:17:04 -0500

    migrate bd to pg

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 15.times do
   fake_html_content = ""
-  Faker::Hipster.paragraphs(rand(2..8)).each do |s|
+  Faker::Hipster.paragraphs(number: rand(2..8)).each do |s|
     fake_html_content +=  "<p>#{s}</p>"
   end
 
   Post.create({
-    title: Faker::Hipster.sentence(rand(2..5)),
+    title: Faker::Hipster.sentence(word_count: rand(2..5)),
     content: fake_html_content,
     published_at: Time.now - rand(1..20).days
   })
